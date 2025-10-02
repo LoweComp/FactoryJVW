eb281dd3a2a7e3ecce81eac152f8386578cb3df0
LoweComp <terci_300@outlook.com>
2025-10-02 12:12:07 -0300

    Testes das Classes Novas

---
+++ src/test/java/com/factoryjvw/EventoBeneficenteTest.java
@@ -0,0 +1,20 @@
+package com.factoryjvw;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventoBeneficenteTest {
+
+    @Test
+    void devePromoverEventoBeneficente() {
+        IEvento evento = EventoFactory.criarEvento("EventoBeneficente");
+        assertEquals("Promoção de Evento Beneficente iniciada: ONGs e Movimentos Sociais contatados", evento.iniciarPromocao());
+    }
+    @Test
+    void deveCancelarShowSolo() {
+        IEvento evento = EventoFactory.criarEvento("EventoBeneficente");
+        assertEquals("Evento Beneficente cancelado e doações destinadas aos colaboradores", evento.cancelarEvento());
+    }
+
+}
